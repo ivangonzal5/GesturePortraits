d5e28a8317e1ba34ef6a37d31f8453e39a10d5d0
losarkar <70538222+losarkar@users.noreply.github.com>
2021-11-01 15:37:50 -0700

    Added UI

---
+++ Assets/Scripts/VolumeSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class VolumeSet : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetLevel(float Value)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(Value) * 20);
+    }
+}
